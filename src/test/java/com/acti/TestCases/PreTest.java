fa35cb27b3642bb9e74ab0a4a0f0035e463db705
91984 <91984@LAPTOP-L43D0T59>
2021-04-22 23:08:29 +0530

    added taking screenshot method

--- src/test/java/com/acti/TestCases/PreTest.java
+++ src/test/java/com/acti/TestCases/PreTest.java
@@ -1,14 +1,19 @@
 package com.acti.TestCases;
 
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.acti.Base.DriverScript;
+import com.acti.Helper.General;
 import com.acti.Pages.EnterTimePage;
 import com.acti.Pages.LoginPage;
 import com.acti.Pages.UserListPage;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 
@@ -44,8 +49,23 @@ public class PreTest extends DriverScript {
 	}
 	
 	@AfterMethod
-	public void tearDown()
+	public void tearDown(ITestResult result)
 	{
+		
+		try{
+			if (result.getStatus()== ITestResult.FAILURE)
+			{
+				String screenshotPath = General.getScreenshot (driver, "screenShotName"); 
+				logger.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" Test case FAILED due to below issues:",ExtentColor.RED));
+				logger.fail(result.getThrowable());
+
+				logger.fail("Snapshot below: " + logger.addScreenCaptureFromPath(screenshotPath));
+		}
+		}
+			catch(Exception e)
+			{
+			System.out.println("An exception occured" +e);
+			}
 		driver.close();
 		extent.flush();
 	}
